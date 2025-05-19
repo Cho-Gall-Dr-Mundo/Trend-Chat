19c1c58f25e4b86366c9ad8a79fe7568a38bf0c8
Your Name <doolchong@gmail.com>
2025-05-19 22:49:30 +0900

    init project

---
+++ chat-service/src/main/java/com/trendchat/chatservice/ChatServiceApplication.java
@@ -0,0 +1,19 @@
+package com.trendchat.chatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ChatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServiceApplication.class, args);
+    }
+
+}
---
+++ trend-service/src/test/java/com/trendchat/trendservice/TrendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendchat.trendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
