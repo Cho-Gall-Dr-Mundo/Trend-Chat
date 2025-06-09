7d9b938467b4388475a7fb549ca4be0b9d18c8c9
Your Name <doolchong@gmail.com>
2025-06-09 20:42:12 +0900

    feat: chat-service Security 구현
    1. Security 설정 적용
    2. 공통 인증 모듈로부터 Web-Flux 인증 필터 적용

---
+++ chat-service/src/main/java/com/trendchat/chatservice/config/JwtUtilConfig.java
@@ -0,0 +1,23 @@
+package com.trendchat.chatservice.config;
+
+import com.trendchat.trendchatcommon.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RefreshScope
+@Configuration
+public class JwtUtilConfig {
+
+    @Value("${jwt.token.access.secret.key}")
+    private String accessSecretKey;
+
+    @Value("${jwt.token.refresh.secret.key}")
+    private String refreshSecretKey;
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return new JwtUtil(accessSecretKey, refreshSecretKey);
+    }
+}
