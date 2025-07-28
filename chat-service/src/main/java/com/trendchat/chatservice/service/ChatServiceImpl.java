4ca92b7eb27a9434a04c05553857727a5c61bbd9
tae98 <98rlaxogus@gmail.com>
2025-07-28 16:22:07 +0900

    feat: 메시지 전동시 채팅방 멤버 검증로직 추가

--- chat-service/src/main/java/com/trendchat/chatservice/service/ChatServiceImpl.java
+++ chat-service/src/main/java/com/trendchat/chatservice/service/ChatServiceImpl.java
@@ -7,8 +7,10 @@ import com.trendchat.chatservice.dto.RoomSummaryEvent;
 import com.trendchat.chatservice.entity.ChatMessage;
 import com.trendchat.chatservice.entity.ChatRoom;
 import com.trendchat.chatservice.repository.ChatMessageRepository;
+import com.trendchat.chatservice.repository.ChatRoomMemberRepository;
 import com.trendchat.chatservice.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +25,7 @@ public class ChatServiceImpl implements ChatService{
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMessagePublisher publisher;
     private final RedisPublisher redisPublisher;
+    private final ChatRoomMemberRepository chatRoomMemberRepository;
 
     @Override
     @Transactional
@@ -49,23 +52,30 @@ public class ChatServiceImpl implements ChatService{
     public void handleMessage(ChatMessageRequest messageRequest) {
         // 1. 채팅방 ID로 ChatRoom 엔티티 찾기
         ChatRoom chatRoom = chatRoomRepository.findById(messageRequest.roomId()).orElseThrow(()-> new IllegalArgumentException("Room not found"));
-
-        // 2. ChatMessage 생성 & 채팅방 연관 설정
+        // 2. 채팅방에 멤버가 아닌지 확인
+        validateMember(messageRequest.sender(),messageRequest.roomId());
+        // 3. ChatMessage 생성 & 채팅방 연관 설정
         ChatMessageDto messageDto = new ChatMessageDto(
                 chatRoom.getId(),
                 messageRequest.sender(),
                 messageRequest.senderNickName(),
                 messageRequest.content()
         );
-        // 3. 메시지 전송 (RabbitMQ → WebFlux 처리)
+        // 4. 메시지 전송 (RabbitMQ → WebFlux 처리)
         publisher.send(messageDto);
 
-        // 4. NEW 알림용 Redis Publish
+        // 5. NEW 알림용 Redis Publish
         RoomSummaryEvent event = new RoomSummaryEvent(
                 chatRoom.getId(),
                 "NEW",
                 LocalDateTime.now()
         );
         redisPublisher.publishRoomMessage(event);
+    }
+    private void validateMember(String userId, Long roomId) throws AccessDeniedException {
+        boolean isMember = chatRoomMemberRepository.existsByChatRoomIdAndUserId(roomId, userId);
+        if(!isMember){
+            throw new AccessDeniedException("해당 채팅방에 멤버가 아닙니다");
+        }
     }
 }
